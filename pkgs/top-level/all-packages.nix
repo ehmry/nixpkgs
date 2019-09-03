e3bcf6586d10f8200b5f7d7c2865b26684353498
Emery Hemingway <ehmry@posteo.net>
2019-09-03 16:00:15 +0200

    Use LLVM 8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8115,7 +8115,7 @@ in
   llvm_38 = llvmPackages_38.llvm;
   llvm_35 = llvmPackages_35.llvm;
 
-  llvmPackages = recurseIntoAttrs llvmPackages_7;
+  llvmPackages = recurseIntoAttrs llvmPackages_8;
 
   llvmPackages_35 = callPackage ../development/compilers/llvm/3.5 ({
     isl = isl_0_14;
