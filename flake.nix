8c371ffe15e1318b564690348588cae5e430e94c
Emery Hemingway <ehmry@posteo.net>
2019-11-10 19:19:39 +0100

    Modify flake for x86_64-genode

--- flake.nix
+++ flake.nix
@@ -6,43 +6,21 @@
   outputs = { self }:
     let
 
-      jobs = import ./pkgs/top-level/release.nix {
-        nixpkgs = self;
-      };
-
       lib = import ./lib;
 
-      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" ];
+      jobs = import ./pkgs/top-level/release.nix { nixpkgs = self; };
+
+      systems = [ "x86_64-genode" ];
 
       forAllSystems = f: lib.genAttrs systems (system: f system);
 
-    in
-    {
-      lib = lib // {
-        nixosSystem = { modules, ... } @ args:
-          import ./nixos/lib/eval-config.nix (args // {
-            modules = modules ++
-              [ { system.nixos.versionSuffix =
-                    ".${lib.substring 0 8 self.lastModified}.${self.shortRev}";
-                  system.nixos.revision = self.rev;
-                }
-              ];
-          });
-      };
+    in {
+      inherit lib;
 
-      checks.tarball = jobs.tarball;
-
-      htmlDocs = {
-        nixpkgsManual = jobs.manual;
-        nixosManual = (import ./nixos/release-small.nix {
-          nixpkgs = self;
-        }).nixos.manual.x86_64-linux;
-      };
-
-      legacyPackages = forAllSystems (system: import ./. { inherit system; });
-
-      nixosModules = {
-        notDetected = ./nixos/modules/installer/scan/not-detected.nix;
-      };
+      legacyPackages = forAllSystems (system:
+        import ./. {
+          localSystem = "x86_64-linux";
+          crossSystem = system;
+        });
     };
 }
