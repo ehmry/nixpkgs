1bb2e7188ea6004abde02bceb2bd64c700e4cddd
Emery Hemingway <ehmry@posteo.net>
2023-10-31 16:17:56 +0000

    c2nim: move out of nimPackages

--- pkgs/top-level/nim-packages.nix
+++ pkgs/top-level/nim-packages.nix
@@ -21,8 +21,6 @@ lib.makeScope newScope (self:
 
     bumpy = callPackage ../development/nim-packages/bumpy { };
 
-    c2nim = callPackage ../development/nim-packages/c2nim { };
-
     cbor = callPackage ../development/nim-packages/cbor { };
 
     chroma = callPackage ../development/nim-packages/chroma { };
